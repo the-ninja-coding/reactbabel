9fc5c520f50e717eae1569bb48777d1323b6d172
Andrescarnederes33 <webmastercloudming@gmail.com>
2020-08-21 15:51:52 -0500

    primer commit

---
+++ src/components/Jsx.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import "../assets/styles/App.scss";
+
+function Jsx() {
+	return (
+		<Fragment>
+			<h1>Jsx Sintax</h1>
+			<p>Este es mi primer componente</p>
+		</Fragment>
+	);
+}
+
+export default Jsx;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Jsx from "./components/Jsx";
+
+ReactDOM.render(<Jsx />, document.getElementById("app"));
